298dc24fbad69e3501a3a3e7f5c55fed9f59f87d
web3nomad.eth <me@web3nomad.eth>
2024-02-08 12:19:37 +1100

    import icons to web app

--- apps/web/next.config.mjs
+++ apps/web/next.config.mjs
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
+  images: {
+    // Image Optimization using the default loader is not compatible with `{ output: 'export' }`
+    unoptimized: true
+  }
 };
 
 export default nextConfig;
--- apps/web/src/app/library/Files.tsx
+++ apps/web/src/app/library/Files.tsx
@@ -1,5 +1,7 @@
 import { useCallback, useEffect, useState } from "react";
 import type { File } from "./types";
+import Image from "next/image";
+import { Folder_Light } from "@muse/assets/icons";
 
 type Props = {
   files: File[]
@@ -14,6 +16,7 @@ export default function Files({ files }: Props) {
             key={file.name}
             className="w-36 h-36 border-2 border-neutral-800 m-2 text-xs flex flex-col justify-between"
           >
+            <Image src={Folder_Light} alt="folder"></Image>
             <span>{file.name}</span>
           </div>
         ))}
