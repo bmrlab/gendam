2bf98fd01b9d0a8b4da353a61f9b71b2d51762d0
web3nomad.eth <me@web3nomad.eth>
2024-02-18 15:22:12 +0800

    remove dynamic import layout

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -1,20 +1,21 @@
-import type { Metadata } from "next";
+"use client";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import dynamic from 'next/dynamic';
-import { client, queryClient, rspc } from "@/lib/rspc";
-
-const ClientLayout = dynamic(() => import('./ClientLayout'), {
-  loading: () => <div className="w-screen h-screen bg-white flex items-center justify-center">Loading...</div>,
-  ssr: false,
-});
 
 const inter = Inter({ subsets: ["latin"] });
 
-export const metadata: Metadata = {
-  title: "Muse | a local DAM of videos",
-  description: "Muse is a local DAM for videos",
-};
+import ClientLayout from "./ClientLayout";
+// import dynamic from 'next/dynamic';
+// const ClientLayout = dynamic(() => import('./ClientLayout'), {
+//   loading: () => <div className="w-screen h-screen bg-white flex items-center justify-center">Loading...</div>,
+//   ssr: false,
+// });
+
+// import type { Metadata } from "next";
+// export const metadata: Metadata = {
+//   title: "Muse | a local DAM of videos",
+//   description: "Muse is a local DAM for videos",
+// };
 
 export default function RootLayout({
   children,
