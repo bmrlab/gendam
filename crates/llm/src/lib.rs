bedf51ba297809cc4ec7fbc2496d44bb396bb173
zhuojg <zhuojg1519@outlook.com>
2024-03-05 11:44:56 +0800

    optimize llm code

--- crates/llm/src/lib.rs
+++ crates/llm/src/lib.rs
@@ -24,16 +24,10 @@ pub enum LLMMessage {
     Assistant(String),
 }
 
-pub enum LLMResult {
-    Text(String),
-    Error,
-}
-
 pub struct LLMInput {
     history: Vec<LLMMessage>,
     ctx_params: Option<LlamaContextParams>,
-    // sampler: Option<StandardSampler>,
-    tx: oneshot::Sender<LLMResult>,
+    tx: oneshot::Sender<anyhow::Result<String>>,
 }
 
 pub enum LLMPayload {
@@ -61,102 +55,34 @@ impl LLM {
             ..Default::default()
         });
         let input_model = model.clone();
-
         let model_path = download.download_if_not_exists(model_uri).await?;
 
         let model = LlamaModel::load_from_file(&backend, model_path, &model_params)?;
 
         let (tx, mut rx) = mpsc::channel::<LLMPayload>(512);
 
+        // start a new local thread where model will run, and make sure model will never be moved
         let rt = tokio::runtime::Builder::new_current_thread()
             .enable_all()
             .build()?;
-
         std::thread::spawn(move || {
             let local = tokio::task::LocalSet::new();
-
             local.spawn_local(async move {
                 loop {
                     while let Some(payload) = rx.recv().await {
                         match payload {
                             LLMPayload::Input(input) => {
-                                let ctx_params = match input.ctx_params {
-                                    Some(params) => params,
-                                    _ => LlamaContextParams::default(),
-                                }
-                                .with_n_ctx(NonZeroU32::new(model.n_ctx_train() as u32))
-                                .with_n_batch(model.n_ctx_train() as u32);
-
-                                let mut ctx = model
-                                    .new_context(&backend, ctx_params)
-                                    .expect("failed to create context");
-
                                 let prompt = input_model.with_chat_template(input.history);
-
                                 debug!("final prompt: {}", prompt);
 
-                                let tokens_list = model
-                                    .str_to_token(&prompt, AddBos::Always)
-                                    .expect("failed to tokenize prompt");
-
-                                let n_len = model.n_ctx_train() as i32;
-
-                                let mut results = vec![];
-
-                                let mut batch = LlamaBatch::new(model.n_ctx_train() as usize, 1);
-
-                                let last_index: i32 = (tokens_list.len() - 1) as i32;
-                                for (i, token) in (0_i32..).zip(tokens_list.into_iter()) {
-                                    // llama_decode will output logits only for the last token of the prompt
-                                    let is_last = i == last_index;
-                                    batch
-                                        .add(token, i, &[0], is_last)
-                                        .expect("failed to add token");
-                                }
-
-                                ctx.decode(&mut batch).expect("failed to decode tokens");
-
-                                let mut n_cur = batch.n_tokens();
-
-                                while n_cur <= n_len {
-                                    {
-                                        let candidates = ctx.candidates_ith(batch.n_tokens() - 1);
-
-                                        let candidates_p =
-                                            LlamaTokenDataArray::from_iter(candidates, false);
-
-                                        // sample the most likely token
-                                        let new_token_id = ctx.sample_token_greedy(candidates_p);
-
-                                        // is it an end of stream?
-                                        if new_token_id == model.token_eos() {
-                                            eprintln!();
-                                            break;
-                                        }
-
-                                        match model.token_to_str(new_token_id) {
-                                            Ok(token) => {
-                                                results.push(token);
-                                            }
-                                            _ => {
-                                                warn!(
-                                                    "failed to convert token to str: {}",
-                                                    new_token_id
-                                                );
-                                            }
-                                        };
-
-                                        batch.clear();
-                                        batch
-                                            .add(new_token_id, n_cur, &[0], true)
-                                            .expect("failed to add token");
+                                match get_completion(prompt, input.ctx_params, &model, &backend) {
+                                    Ok(result) => {
+                                        let _ = input.tx.send(Ok(result));
                                     }
-                                    n_cur += 1;
-
-                                    ctx.decode(&mut batch).expect("failed to decode tokens");
+                                    Err(e) => {
+                                        let _ = input.tx.send(Err(e));
+                                    }
                                 }
-
-                                let _ = input.tx.send(LLMResult::Text(results.join("")));
                             }
                             _ => {
                                 todo!()
@@ -177,7 +103,7 @@ impl LLM {
         history: Vec<LLMMessage>,
         ctx_params: Option<LlamaContextParams>,
     ) -> anyhow::Result<String> {
-        let (tx, rx) = oneshot::channel::<LLMResult>();
+        let (tx, rx) = oneshot::channel::<anyhow::Result<String>>();
 
         self.tx
             .send(LLMPayload::Input(LLMInput {
@@ -188,12 +114,98 @@ impl LLM {
             .await?;
 
         match rx.await {
-            Ok(LLMResult::Text(text)) => Ok(text),
-            Ok(LLMResult::Error) => bail!("error from LLM"),
+            Ok(Ok(text)) => Ok(text),
+            Ok(Err(e)) => bail!("error from LLM: {}", e),
             Err(e) => {
                 error!("channel error");
                 Err(e.into())
             }
+        }
+    }
+}
+
+fn get_completion(
+    prompt: String,
+    ctx_params: Option<LlamaContextParams>,
+    model: &LlamaModel,
+    backend: &LlamaBackend,
+) -> anyhow::Result<String> {
+    let ctx_params = match ctx_params {
+        Some(params) => params,
+        // TODO not sure if it is ok to use n_ctx_train as default n_ctx and n_batch
+        None => LlamaContextParams::default()
+            .with_n_ctx(NonZeroU32::new(model.n_ctx_train() as u32))
+            .with_n_batch(model.n_ctx_train() as u32),
+    };
+
+    match model.new_context(&backend, ctx_params) {
+        Ok(mut ctx) => {
+            let tokens_list = model
+                .str_to_token(&prompt, AddBos::Always)
+                .expect("failed to tokenize prompt");
+
+            let n_len = model.n_ctx_train() as i32;
+
+            let mut results = vec![];
+
+            let mut batch = LlamaBatch::new(model.n_ctx_train() as usize, 1);
+
+            let last_index: i32 = (tokens_list.len() - 1) as i32;
+            for (i, token) in (0_i32..).zip(tokens_list.into_iter()) {
+                // llama_decode will output logits only for the last token of the prompt
+                let is_last = i == last_index;
+                if let Err(e) = batch.add(token, i, &[0], is_last) {
+                    warn!("failed to add token: {}", e);
+                }
+            }
+
+            if let Err(e) = ctx.decode(&mut batch) {
+                warn!("failed to decode tokens: {}", e);
+            }
+
+            let mut n_cur = batch.n_tokens();
+
+            while n_cur <= n_len {
+                {
+                    let candidates = ctx.candidates_ith(batch.n_tokens() - 1);
+
+                    let candidates_p = LlamaTokenDataArray::from_iter(candidates, false);
+
+                    // sample the most likely token
+                    let new_token_id = ctx.sample_token_greedy(candidates_p);
+
+                    // is it an end of stream?
+                    if new_token_id == model.token_eos() {
+                        break;
+                    }
+
+                    match model.token_to_str(new_token_id) {
+                        Ok(token) => {
+                            results.push(token);
+                        }
+                        _ => {
+                            warn!("failed to convert token to str: {}", new_token_id);
+                        }
+                    };
+
+                    batch.clear();
+                    if let Err(e) = batch.add(new_token_id, n_cur, &[0], true) {
+                        warn!("failed to add token: {}", e)
+                    }
+                }
+                n_cur += 1;
+
+                if let Err(e) = ctx.decode(&mut batch) {
+                    warn!("failed to decode batch: {}", e);
+                }
+            }
+
+            // let _ = input.tx.send(Ok(results.join("")));
+            Ok(results.join(""))
+        }
+        Err(e) => {
+            bail!("failed to create context: {}", e);
+            // let _ = input.tx.send(Err(e.into()));
         }
     }
 }
