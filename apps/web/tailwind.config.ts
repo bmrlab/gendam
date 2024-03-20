937dbbb0688fe5638c70bdc908d323cd04fa66f7
ZingerLittleBee <6970999@gmail.com>
2024-03-20 19:25:41 +0800

    fix(task-list): tw jit

--- apps/web/tailwind.config.ts
+++ apps/web/tailwind.config.ts
@@ -2,7 +2,13 @@ import type { Config } from 'tailwindcss'
 
 const config = {
   darkMode: ['class'],
-  content: ['./pages/**/*.{ts,tsx}', './components/**/*.{ts,tsx}', './app/**/*.{ts,tsx}', './src/**/*.{ts,tsx}'],
+  content: [
+    '../../packages/ui//src/**/*.{ts,tsx}',
+    './pages/**/*.{ts,tsx}',
+    './components/**/*.{ts,tsx}',
+    './app/**/*.{ts,tsx}',
+    './src/**/*.{ts,tsx}',
+  ],
   prefix: '',
   theme: {
     container: {
