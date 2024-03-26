d6013befc2c1ad8412a4660ce108c740eb8149d5
web3nomad.eth <me@web3nomad.eth>
2024-03-26 16:20:01 +0800

    modify ExplorerItem to allow undefined on assetObject and mediaData

--- apps/web/src/Explorer/types.ts
+++ apps/web/src/Explorer/types.ts
@@ -3,10 +3,10 @@ export type ExplorerItem = {
   name: string
   materializedPath: string
   isDir: boolean
-  assetObject: {
+  assetObject?: {
     id: number;
     hash: string;
-    mediaData: {
+    mediaData?: {
       width: number;
       height: number;
       duration: number;
@@ -17,3 +17,9 @@ export type ExplorerItem = {
   createdAt: string
   updatedAt: string
 }
+
+/**
+ * FilePath 上面没有 assetObject，主要是 prisma.rs 里面对这个字段设置了 #[specta(skip)]，
+ * 但实际返回数据里面这个字段改有的时候还是会有，
+ * 这里就先加上 ?，除了 null，还允许 undefined
+ */
