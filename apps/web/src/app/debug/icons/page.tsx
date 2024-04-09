a6dc83f7d8199783cde0975d77492c835b14b811
web3nomad.eth <me@web3nomad.eth>
2024-04-09 21:37:24 +0800

    add icons debug page

---
+++ apps/web/src/app/debug/icons/page.tsx
@@ -0,0 +1,56 @@
+import Icon from '@muse/ui/icons'
+
+export default function Debug() {
+  return (
+    <div className='h-screen flex-1 overflow-auto'>
+      <div className='flex items-center gap-2'><Icon.Trash className="w-4 h-4" /> Trash</div>
+      <div className='flex items-center gap-2'><Icon.Upload className="w-4 h-4" /> Upload</div>
+      <div className='flex items-center gap-2'><Icon.Download className="w-4 h-4" /> Download</div>
+      <div className='flex items-center gap-2'><Icon.Compass className="w-4 h-4" /> Compass</div>
+      <div className='flex items-center gap-2'><Icon.File className="w-4 h-4" /> File</div>
+      <div className='flex items-center gap-2'><Icon.Briefcase className="w-4 h-4" /> Briefcase</div>
+      <div className='flex items-center gap-2'><Icon.Rocket className="w-4 h-4" /> Rocket</div>
+      <div className='flex items-center gap-2'><Icon.Gear className="w-4 h-4" /> Gear</div>
+      <div className='flex items-center gap-2'><Icon.SpeakerSimpleX className="w-4 h-4" /> SpeakerSimpleX</div>
+      <div className='flex items-center gap-2'><Icon.SpeakerHigh className="w-4 h-4" /> SpeakerHigh</div>
+      <div className='flex items-center gap-2'><Icon.Play className="w-4 h-4" /> Play</div>
+      <div className='flex items-center gap-2'><Icon.SkipBack className="w-4 h-4" /> SkipBack</div>
+      <div className='flex items-center gap-2'><Icon.SkipForward className="w-4 h-4" /> SkipForward</div>
+      <div className='flex items-center gap-2'><Icon.Mic className="w-4 h-4" /> Mic</div>
+      <div className='flex items-center gap-2'><Icon.Close className="w-4 h-4" /> Close</div>
+      <div className='flex items-center gap-2'><Icon.Check className="w-4 h-4" /> Check</div>
+      <div className='flex items-center gap-2'><Icon.CloseRounded className="w-4 h-4" /> CloseRounded</div>
+      <div className='flex items-center gap-2'><Icon.Cycle className="w-4 h-4" /> Cycle</div>
+      <div className='flex items-center gap-2'><Icon.Image className="w-4 h-4" /> Image</div>
+      <div className='flex items-center gap-2'><Icon.Loading className="w-4 h-4" /> Loading</div>
+      <div className='flex items-center gap-2'><Icon.MoreVertical className="w-4 h-4" /> MoreVertical</div>
+      <div className='flex items-center gap-2'><Icon.MoreHorizontal className="w-4 h-4" /> MoreHorizontal</div>
+      <div className='flex items-center gap-2'><Icon.Grid className="w-4 h-4" /> Grid</div>
+      <div className='flex items-center gap-2'><Icon.List className="w-4 h-4" /> List</div>
+      <div className='flex items-center gap-2'><Icon.Column className="w-4 h-4" /> Column</div>
+      <div className='flex items-center gap-2'><Icon.Gallery className="w-4 h-4" /> Gallery</div>
+      <div className='flex items-center gap-2'><Icon.UpAndDownArrow className="w-4 h-4" /> UpAndDownArrow</div>
+      <div className='flex items-center gap-2'><Icon.ArrowUp className="w-4 h-4" /> ArrowUp</div>
+      <div className='flex items-center gap-2'><Icon.ArrowRight className="w-4 h-4" /> ArrowRight</div>
+      <div className='flex items-center gap-2'><Icon.ArrowDown className="w-4 h-4" /> ArrowDown</div>
+      <div className='flex items-center gap-2'><Icon.ArrowLeft className="w-4 h-4" /> ArrowLeft</div>
+      <div className='flex items-center gap-2'><Icon.ScreenExpand className="w-4 h-4" /> ScreenExpand</div>
+      <div className='flex items-center gap-2'><Icon.ScreenNarrow className="w-4 h-4" /> ScreenNarrow</div>
+      <div className='flex items-center gap-2'><Icon.MagnifyingGlass className="w-4 h-4" /> MagnifyingGlass</div>
+      <div className='flex items-center gap-2'><Icon.Clock className="w-4 h-4" /> Clock</div>
+      <div className='flex items-center gap-2'><Icon.Add className="w-4 h-4" /> Add</div>
+      <div className='flex items-center gap-2'><Icon.Minus className="w-4 h-4" /> Minus</div>
+      <div className='flex items-center gap-2'><Icon.FolderOpen className="w-4 h-4" /> FolderOpen</div>
+      <div className='flex items-center gap-2'><Icon.ArrowClockwise className="w-4 h-4" /> ArrowClockwise</div>
+      <div className='flex items-center gap-2'><Icon.EditSquare className="w-4 h-4" /> EditSquare</div>
+      <div className='flex items-center gap-2'><Icon.RemoveSquare className="w-4 h-4" /> RemoveSquare</div>
+      <div className='flex items-center gap-2'><Icon.Finder className="w-4 h-4" /> Finder</div>
+      <div className='flex items-center gap-2'><Icon.RemoveFromList className="w-4 h-4" /> RemoveFromList</div>
+      <div className='flex items-center gap-2'><Icon.CopySimple className="w-4 h-4" /> CopySimple</div>
+      <div className='flex items-center gap-2'><Icon.ArrowUpLeft className="w-4 h-4" /> ArrowUpLeft</div>
+      <div className='flex items-center gap-2'><Icon.QuestionMark className="w-4 h-4" /> QuestionMark</div>
+      <div className='flex items-center gap-2'><Icon.Tag className="w-4 h-4" /> Tag</div>
+      <div className='flex items-center gap-2'><Icon.EditPen className="w-4 h-4" /> EditPen</div>
+    </div>
+  )
+}
