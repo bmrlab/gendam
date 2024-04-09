2fd3e59e97dd1e5f2c228c71d3caadc5730fedc1
web3nomad.eth <me@web3nomad.eth>
2024-04-09 21:29:46 +0800

    add svgr cli

---
+++ packages/ui/src/icons/README.md
@@ -0,0 +1,5 @@
+Generate JSX from SVG File
+
+```bash
+npx @svgr/cli --icon 16px --ext jsx -d ./jsx ./svg/Add.svg
+```
---
+++ packages/ui/src/icons/index.tsx
@@ -0,0 +1,7 @@
+import * as React from 'react'
+
+export type SvgIconProps = React.SVGProps<SVGSVGElement>
+
+const Icon = {
+
+}
