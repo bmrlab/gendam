d7f5cb2de67eed124413df6f253964957c3e346b
web3nomad.eth <me@web3nomad.eth>
2024-04-09 21:35:17 +0800

    export icons

--- packages/ui/src/icons/index.tsx
+++ packages/ui/src/icons/index.tsx
@@ -1,7 +1,102 @@
 import * as React from 'react'
+import Add from './jsx/Add'
+import ArrowClockwise from './jsx/ArrowClockwise'
+import ArrowDown from './jsx/ArrowDown'
+import ArrowLeft from './jsx/ArrowLeft'
+import ArrowRight from './jsx/ArrowRight'
+import ArrowUp from './jsx/ArrowUp'
+import ArrowUpLeft from './jsx/ArrowUpLeft'
+import Briefcase from './jsx/Briefcase'
+import Check from './jsx/Check'
+import Clock from './jsx/Clock'
+import Close from './jsx/Close'
+import CloseRounded from './jsx/CloseRounded'
+import Column from './jsx/Column'
+import Compass from './jsx/Compass'
+import CopySimple from './jsx/CopySimple'
+import Cycle from './jsx/Cycle'
+import Download from './jsx/Download'
+import EditPen from './jsx/EditPen'
+import EditSquare from './jsx/EditSquare'
+import File from './jsx/File'
+import Finder from './jsx/Finder'
+import FolderOpen from './jsx/FolderOpen'
+import Gallery from './jsx/Gallery'
+import Gear from './jsx/Gear'
+import Grid from './jsx/Grid'
+import Image from './jsx/Image'
+import List from './jsx/List'
+import Loading from './jsx/Loading'
+import MagnifyingGlass from './jsx/MagnifyingGlass'
+import Mic from './jsx/Mic'
+import Minus from './jsx/Minus'
+import MoreHorizontal from './jsx/MoreHorizontal'
+import MoreVertical from './jsx/MoreVertical'
+import Play from './jsx/Play'
+import QuestionMark from './jsx/QuestionMark'
+import RemoveFromList from './jsx/RemoveFromList'
+import RemoveSquare from './jsx/RemoveSquare'
+import Rocket from './jsx/Rocket'
+import ScreenExpand from './jsx/ScreenExpand'
+import ScreenNarrow from './jsx/ScreenNarrow'
+import SkipBack from './jsx/SkipBack'
+import SkipForward from './jsx/SkipForward'
+import SpeakerHigh from './jsx/SpeakerHigh'
+import SpeakerSimpleX from './jsx/SpeakerSimpleX'
+import Tag from './jsx/Tag'
+import Trash from './jsx/Trash'
+import UpAndDownArrow from './jsx/UpAndDownArrow'
+import Upload from './jsx/Upload'
 
 export type SvgIconProps = React.SVGProps<SVGSVGElement>
 
-const Icon = {
-
+export default {
+  Add,
+  ArrowClockwise,
+  ArrowDown,
+  ArrowLeft,
+  ArrowRight,
+  ArrowUp,
+  ArrowUpLeft,
+  Briefcase,
+  Check,
+  Clock,
+  Close,
+  CloseRounded,
+  Column,
+  Compass,
+  CopySimple,
+  Cycle,
+  Download,
+  EditPen,
+  EditSquare,
+  File,
+  Finder,
+  FolderOpen,
+  Gallery,
+  Gear,
+  Grid,
+  Image,
+  List,
+  Loading,
+  MagnifyingGlass,
+  Mic,
+  Minus,
+  MoreHorizontal,
+  MoreVertical,
+  Play,
+  QuestionMark,
+  RemoveFromList,
+  RemoveSquare,
+  Rocket,
+  ScreenExpand,
+  ScreenNarrow,
+  SkipBack,
+  SkipForward,
+  SpeakerHigh,
+  SpeakerSimpleX,
+  Tag,
+  Trash,
+  UpAndDownArrow,
+  Upload,
 }
