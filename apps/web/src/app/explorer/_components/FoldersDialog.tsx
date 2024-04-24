934f277b5c8de9e0a1abff24578bb6cad1ce868e
web3nomad.eth <me@web3nomad.eth>
2024-04-24 13:54:54 +0800

    modify text on foldersdialog

--- apps/web/src/app/explorer/_components/FoldersDialog.tsx
+++ apps/web/src/app/explorer/_components/FoldersDialog.tsx
@@ -94,10 +94,10 @@ export function FoldersDialog({ onConfirm }: { onConfirm: (path: ExplorerItem | 
         <Dialog.Overlay onClick={(e) => e.stopPropagation()} />
         <Dialog.Content onClick={(e) => e.stopPropagation()} className="w-[40rem]">
           <div className="flex items-center justify-start border-b border-app-line px-4 py-3">
-            <div className="text-sm">选择文件夹</div>
+            <div className="text-sm">Choose folder</div>
             <div className="ml-2 flex items-center gap-1 text-xs text-ink/50" onClick={() => goto('-1')}>
               <Icon.arrowUpLeft className="h-4 w-4" />
-              <span>返回上一级</span>
+              <span>Back</span>
             </div>
           </div>
           <div className="flex h-80 flex-wrap content-start items-start justify-start gap-4 overflow-auto p-4">
@@ -115,7 +115,7 @@ export function FoldersDialog({ onConfirm }: { onConfirm: (path: ExplorerItem | 
                 </div>
               ))
             ) : (
-              <div className="w-full py-8 text-center text-xs text-ink/50">当前文件夹为空</div>
+              <div className="w-full py-8 text-center text-xs text-ink/50">No folders</div>
             )}
           </div>
           <div className="flex items-center justify-end gap-2 border-t border-app-line px-4 py-2 text-sm">
@@ -125,10 +125,10 @@ export function FoldersDialog({ onConfirm }: { onConfirm: (path: ExplorerItem | 
             <div className="text-xs">{currentPath}</div>
             <div className="mr-auto"></div>
             <Dialog.Close asChild>
-              <Button variant="outline" size="sm">取消</Button>
+              <Button variant="outline" size="sm">Cancel</Button>
             </Dialog.Close>
             <Dialog.Close asChild onClick={() => onConfirm(currentExplorerItem)}>
-              <Button variant="accent" size="sm">选择当前文件夹</Button>
+              <Button variant="accent" size="sm">Choose current folder</Button>
             </Dialog.Close>
             {/* {Array.from(explorer.selectedItems).map((item) => {
               return (
