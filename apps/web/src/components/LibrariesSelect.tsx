7946af470d3ec3d990ad2db939144cf0674c1e5f
web3nomad.eth <me@web3nomad.eth>
2024-04-26 14:44:51 +0800

    input library title

--- apps/web/src/components/LibrariesSelect.tsx
+++ apps/web/src/components/LibrariesSelect.tsx
@@ -3,24 +3,35 @@ import { queryClient, rspc } from '@/lib/rspc'
 import { GenDAM_Logo } from '@muse/assets/images'
 import Icon from '@muse/ui/icons'
 import Image from 'next/image'
-import { useCallback } from 'react'
+import { useCallback, useState } from 'react'
+import { Dialog } from '@muse/ui/v2/dialog'
+import { Form } from '@muse/ui/v2/form'
+import { Button } from '@muse/ui/v2/button'
 
 export default function LibrariesSelect({
   switchCurrentLibraryById,
 }: {
   switchCurrentLibraryById: (libraryId: string) => Promise<void>
 }) {
+  const [dialogOpen, setDialogOpen] = useState(false)
+  const [title, setTitle] = useState('')
   const librariesQuery = rspc.useQuery(['libraries.list'])
   const libraryCreateMut = rspc.useMutation('libraries.create')
 
-  const createLibrary = useCallback(() => {
-    libraryCreateMut.mutate('a test library', {
-      onSuccess: () =>
+  const onSubmit = useCallback(
+    async (e: React.FormEvent<HTMLFormElement>) => {
+      e.preventDefault()
+      try {
+        await libraryCreateMut.mutateAsync(title)
+        setDialogOpen(false)
+        setTitle('')
         queryClient.invalidateQueries({
           queryKey: ['libraries.list'],
-        }),
-    })
-  }, [libraryCreateMut])
+        })
+      } catch(error: any) {}
+    },
+    [libraryCreateMut, title],
+  )
 
   return (
     <div className="bg-app flex h-screen w-screen flex-col items-center justify-center">
@@ -48,7 +59,7 @@ export default function LibrariesSelect({
               </div>
             )
           })}
-          <div className="hover:bg-app-hover rounded-md px-3 py-2" onClick={() => createLibrary()}>
+          <div className="hover:bg-app-hover rounded-md px-3 py-2" onClick={() => setDialogOpen(true)}>
             <div className="text-center text-sm">Create a library</div>
           </div>
         </div>
@@ -58,6 +69,25 @@ export default function LibrariesSelect({
           <div className="mt-8">Loading library</div>
         </div>
       )}
+      <Dialog.Root open={dialogOpen} onOpenChange={(open) => {
+        setDialogOpen(open)
+        if (!open) setTitle('')
+      }}>
+        <Dialog.Portal>
+          <Dialog.Overlay onClick={(e) => e.stopPropagation()} />
+          <Dialog.Content onClick={(e) => e.stopPropagation()} className="w-96 px-4 pb-6 pt-4">
+            <Form.Root onSubmit={onSubmit}>
+              <Form.Field name="title" className="flex flex-col items-stretch justify-center gap-5">
+                <Form.Label>Library name</Form.Label>
+                <Form.Input size="md" value={title} onChange={(e) => setTitle(e.currentTarget.value)} />
+                <Button type="submit" variant="accent" disabled={libraryCreateMut.isPending}>
+                  Create
+                </Button>
+              </Form.Field>
+            </Form.Root>
+          </Dialog.Content>
+        </Dialog.Portal>
+      </Dialog.Root>
     </div>
   )
 }
