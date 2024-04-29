68db4d7ff31633872c8934c611ef1f3f051a6d7a
web3nomad.eth <me@web3nomad.eth>
2024-04-29 17:09:26 +0800

    modify model download links

--- scripts/download-models.sh
+++ scripts/download-models.sh
@@ -3,17 +3,17 @@
 resources_dir="apps/desktop/src-tauri/resources"
 
 if [ ! -f "${resources_dir}/stella-base-zh-v3-1792d" ]; then
-  curl -L "https://assets.bmr.art/gendam/models/stella-base-zh-v3-1792d.tar.gz" | tar xz -C "${resources_dir}/"
+  curl -L "https://gendam.s3.us-west-1.amazonaws.com/models/stella-base-zh-v3-1792d.tar.gz" | tar xz -C "${resources_dir}/"
 fi
 
 if [ ! -f "${resources_dir}/whisper" ]; then
-  curl -L "https://assets.bmr.art/gendam/models/whisper.tar.gz" | tar xz -C "${resources_dir}/"
+  curl -L "https://gendam.s3.us-west-1.amazonaws.com/models/whisper.tar.gz" | tar xz -C "${resources_dir}/"
 fi
 
 if [ ! -f "${resources_dir}/CLIP-ViT-B-32-multilingual-v1" ]; then
-  curl -L "https://assets.bmr.art/gendam/models/CLIP-ViT-B-32-multilingual-v1.tar.gz" | tar xz -C "${resources_dir}/"
+  curl -L "https://gendam.s3.us-west-1.amazonaws.com/models/CLIP-ViT-B-32-multilingual-v1.tar.gz" | tar xz -C "${resources_dir}/"
 fi
 
 if [ ! -f "${resources_dir}/blip-base" ]; then
-  curl -L "https://assets.bmr.art/gendam/models/blip-base.tar.gz" | tar xz -C "${resources_dir}/"
+  curl -L "https://gendam.s3.us-west-1.amazonaws.com/models/blip-base.tar.gz" | tar xz -C "${resources_dir}/"
 fi
