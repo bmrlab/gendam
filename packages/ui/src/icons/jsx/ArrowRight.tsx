6107daaec8d5f9a9a285cab3db6c11066c12dbc1
web3nomad.eth <me@web3nomad.eth>
2024-05-03 02:43:20 +0800

    remove style="mix-blend-mode:luminosity" from svg icons

--- packages/ui/src/icons/jsx/ArrowRight.tsx
+++ packages/ui/src/icons/jsx/ArrowRight.tsx
@@ -5,9 +5,6 @@ const SvgArrowRight = (props: SVGProps<SVGSVGElement>) => (
     <path
       fill="currentColor"
       d="M5.793 13a.65.65 0 0 0 .468-.192l4.386-4.284c.152-.158.23-.321.23-.524a.68.68 0 0 0-.23-.519L6.26 3.191A.65.65 0 0 0 5.793 3a.66.66 0 0 0-.67.665c0 .18.078.355.202.485l3.952 3.856-3.952 3.844a.69.69 0 0 0-.203.485.66.66 0 0 0 .671.665"
-      style={{
-        mixBlendMode: 'luminosity',
-      }}
     />
   </svg>
 )
