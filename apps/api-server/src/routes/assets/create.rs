7c7501d4df5872d9c5eee7b3f5baae15991fbb01
web3nomad.eth <me@web3nomad.eth>
2024-05-04 17:18:57 +0800

    rename new file name like 'name_wo_ext n[.ext]'

--- apps/api-server/src/routes/assets/create.rs
+++ apps/api-server/src/routes/assets/create.rs
@@ -98,43 +98,62 @@ pub async fn create_asset_object(
                         .create(
                             file_hash.clone(),
                             file_size_in_bytes,
-                            vec![asset_object::mime_type::set(file_mime_type)]
+                            vec![asset_object::mime_type::set(file_mime_type)],
                         )
                         .exec()
                         .await?
                 }
             };
-            let mut new_name = name.to_string();
-            let file_path_data = loop {
-                let res = client
-                    .file_path()
-                    .create(
-                        false,
-                        materialized_path.to_string(),
-                        new_name.clone(),
-                        vec![file_path::asset_object_id::set(Some(asset_object_data.id))],
-                    )
-                    .exec()
-                    .await;
-                if let Err(e) = res {
-                    if e.to_string().contains("Unique constraint failed") {
-                        tracing::info!("failed to create file_path: {}, retry with a new name", e);
-                        let suffix = uuid::Uuid::new_v4()
-                            .to_string()
-                            .split("-")
-                            .next()
-                            .unwrap()
-                            .to_string();
-                        new_name = format!("{} ({})", name, suffix);
-                        continue;
-                    } else {
-                        tracing::error!("failed to create file_path: {}", e);
-                        return Err(e);
-                    }
-                } else {
-                    break res.unwrap();
-                }
+            let (name_wo_ext, ext) = match name.rsplit_once('.') {
+                Some((wo_ext, ext)) => (wo_ext, ext),
+                None => (name, ""),
             };
+            let mut where_clause = vec![
+                file_path::materialized_path::equals(materialized_path.into()),
+                file_path::name::starts_with(name_wo_ext.into()),
+            ];
+            if ext != "" {
+                where_clause.push(file_path::name::ends_with(format!(".{}", ext)));
+            }
+            let matches = client.file_path().find_many(where_clause).exec().await?;
+            // find the max number "n" in format "name_wo_ext n.ext"
+            let max_num = matches
+                .iter()
+                .filter_map(|file_path_data| {
+                    let name = file_path_data.name.as_str();
+                    let (name_wo_ext_1, ext_1) = match name.rsplit_once('.') {
+                        Some((wo_ext, ext)) => (wo_ext, ext),
+                        None => (name, "")
+                    };
+                    if ext_1 == ext && name_wo_ext_1 == name_wo_ext {
+                        return Some(0);
+                    }
+                    let (name_wo_ext_1, num) = match name_wo_ext_1.rsplit_once(' ') {
+                        Some((prefix, num)) => (prefix, num),
+                        None => (name_wo_ext_1, "0"),
+                    };
+                    if ext_1 == ext && name_wo_ext_1 == name_wo_ext {
+                        num.parse::<u32>().ok()  // Converts from Result<T, E> to Option<T>
+                    } else {
+                        None
+                    }
+                })
+                .max();
+            let ext_with_dot = if ext == "" { "".to_string() } else { format!(".{}", ext) };
+            let new_name = match max_num {
+                Some(max_num) => format!("{} {}{}", name_wo_ext, max_num + 1, ext_with_dot),
+                None => format!("{}{}", name_wo_ext, ext_with_dot), // same as name
+            };
+            let file_path_data = client
+                .file_path()
+                .create(
+                    false,
+                    materialized_path.to_string(),
+                    new_name.clone(),
+                    vec![file_path::asset_object_id::set(Some(asset_object_data.id))],
+                )
+                .exec()
+                .await?;
             Ok((asset_object_data, file_path_data, asset_object_existed))
         })
         .await
@@ -147,3 +166,35 @@ pub async fn create_asset_object(
 
     Ok((file_path_data, asset_object_data, asset_object_existed))
 }
+
+// let mut new_name = name.to_string();
+// let file_path_data = loop {
+//     let res = client
+//         .file_path()
+//         .create(
+//             false,
+//             materialized_path.to_string(),
+//             new_name.clone(),
+//             vec![file_path::asset_object_id::set(Some(asset_object_data.id))],
+//         )
+//         .exec()
+//         .await;
+//     if let Err(e) = res {
+//         if e.to_string().contains("Unique constraint failed") {
+//             tracing::info!("failed to create file_path: {}, retry with a new name", e);
+//             let suffix = uuid::Uuid::new_v4()
+//                 .to_string()
+//                 .split("-")
+//                 .next()
+//                 .unwrap()
+//                 .to_string();
+//             new_name = format!("{} ({})", name, suffix);
+//             continue;
+//         } else {
+//             tracing::error!("failed to create file_path: {}", e);
+//             return Err(e);
+//         }
+//     } else {
+//         break res.unwrap();
+//     }
+// };
