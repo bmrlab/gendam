ad4baa65ca2cf6fc41cb327324ec8ffc0c532484
ranbo <71162666+RustLoverTheCoder@users.noreply.github.com>
2024-05-23 16:20:14 +0800

    feat: add image process

--- apps/api-server/src/file_handler/mod.rs
+++ apps/api-server/src/file_handler/mod.rs
@@ -153,35 +153,64 @@ pub fn get_file_handler(
     let library = ctx.library()?;
 
     let handler: Box<dyn FileHandler> = match &asset_object_data.mime_type {
-        Some(mime_type) if mime_type.starts_with("video") => {
-            let handler = VideoHandler::new(&asset_object_data.hash, &library)?;
+        Some(mime_type) => {
+            if mime_type.starts_with("video") {
+                let handler = VideoHandler::new(&asset_object_data.hash, &library)?;
 
-            let library_settings = get_library_settings(&library.dir);
+                let library_settings = get_library_settings(&library.dir);
 
-            let ai_handler = ctx.ai_handler()?;
-            let qdrant_info = ctx.qdrant_info()?;
+                let ai_handler = ctx.ai_handler()?;
+                let qdrant_info = ctx.qdrant_info()?;
 
-            let handler = handler
-                .with_multi_modal_embedding(
-                    ai_handler.multi_modal_embedding.as_ref(),
-                    &library_settings.models.multi_modal_embedding,
-                    &qdrant_info.vision_collection.name,
-                )
-                .with_image_caption(
-                    ai_handler.image_caption.as_ref(),
-                    &library_settings.models.image_caption,
-                )
-                .with_audio_transcript(
-                    ai_handler.audio_transcript.as_ref(),
-                    &library_settings.models.audio_transcript,
-                )
-                .with_text_embedding(
-                    ai_handler.text_embedding.as_ref(),
-                    &library_settings.models.text_embedding,
-                    &qdrant_info.language_collection.name,
-                );
+                let handler = handler
+                    .with_multi_modal_embedding(
+                        ai_handler.multi_modal_embedding.as_ref(),
+                        &library_settings.models.multi_modal_embedding,
+                        &qdrant_info.vision_collection.name,
+                    )
+                    .with_image_caption(
+                        ai_handler.image_caption.as_ref(),
+                        &library_settings.models.image_caption,
+                    )
+                    .with_audio_transcript(
+                        ai_handler.audio_transcript.as_ref(),
+                        &library_settings.models.audio_transcript,
+                    )
+                    .with_text_embedding(
+                        ai_handler.text_embedding.as_ref(),
+                        &library_settings.models.text_embedding,
+                        &qdrant_info.language_collection.name,
+                    );
 
-            Box::new(handler)
+                Box::new(handler)
+            } else if mime_type.starts_with("image") {
+                let handler = VideoHandler::new(&asset_object_data.hash, &library)?;
+
+                let library_settings = get_library_settings(&library.dir);
+
+                let ai_handler = ctx.ai_handler()?;
+                let qdrant_info = ctx.qdrant_info()?;
+
+                let handler = handler
+                    .with_multi_modal_embedding(
+                        ai_handler.multi_modal_embedding.as_ref(),
+                        &library_settings.models.multi_modal_embedding,
+                        &qdrant_info.vision_collection.name,
+                    )
+                    .with_image_caption(
+                        ai_handler.image_caption.as_ref(),
+                        &library_settings.models.image_caption,
+                    )
+                    .with_text_embedding(
+                        ai_handler.text_embedding.as_ref(),
+                        &library_settings.models.text_embedding,
+                        &qdrant_info.language_collection.name,
+                    );
+
+                Box::new(handler)
+            } else {
+                bail!("Unsupported mime type: {:?}", &asset_object_data.mime_type)
+            }
         }
         _ => {
             bail!("Unsupported mime type: {:?}", &asset_object_data.mime_type)
--- scripts/download-models.sh
+++ scripts/download-models.sh
@@ -2,18 +2,18 @@
 
 resources_dir="apps/desktop/src-tauri/resources"
 
-if [ ! -f "${resources_dir}/stella-base-zh-v3-1792d" ]; then
+if [ ! -d "${resources_dir}/stella-base-zh-v3-1792d" ]; then
   curl -L "https://gendam.s3.us-west-1.amazonaws.com/models/stella-base-zh-v3-1792d.tar.gz" | tar xz -C "${resources_dir}/"
 fi
 
-if [ ! -f "${resources_dir}/whisper" ]; then
+if [ ! -d "${resources_dir}/whisper" ]; then
   curl -L "https://gendam.s3.us-west-1.amazonaws.com/models/whisper.tar.gz" | tar xz -C "${resources_dir}/"
 fi
 
-if [ ! -f "${resources_dir}/CLIP-ViT-B-32-multilingual-v1" ]; then
+if [ ! -d "${resources_dir}/CLIP-ViT-B-32-multilingual-v1" ]; then
   curl -L "https://gendam.s3.us-west-1.amazonaws.com/models/CLIP-ViT-B-32-multilingual-v1.tar.gz" | tar xz -C "${resources_dir}/"
 fi
 
-if [ ! -f "${resources_dir}/blip-base" ]; then
+if [ ! -d "${resources_dir}/blip-base" ]; then
   curl -L "https://gendam.s3.us-west-1.amazonaws.com/models/blip-base.tar.gz" | tar xz -C "${resources_dir}/"
 fi
