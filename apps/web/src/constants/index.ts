331f5cd9f2db095cf593dc59bbca06f25223b258
ranbo <71162666+RustLoverTheCoder@users.noreply.github.com>
2024-06-06 17:06:24 +0800

    fix: rebase bug
    fix: rebase bug

--- apps/web/src/constants/index.ts
+++ apps/web/src/constants/index.ts
@@ -1,3 +1,3 @@
-export const SUPPORTED_VIDEO_CONTENT_TYPES = new Set(['mp4', 'mov', 'avi', 'mkv'])
+export const SUPPORTED_VIDEO_CONTENT_TYPES = new Set(['avi', 'wmv', 'mkv', 'mp4', 'mov', 'rm', 'mpg', 'rmvb'])
 
 export const SUPPORTED_IMAGE_CONTENT_TYPES = new Set(['jpg', 'jpeg', 'png', 'gif', 'webp'])
