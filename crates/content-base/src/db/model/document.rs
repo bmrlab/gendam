947ba4d40d1bace23ee9c4c3b32f8ad4c05e3f68
ZingerLittleBee <6970999@gmail.com>
2024-09-11 15:46:47 +0800

    replace qdrant

--- crates/content-base/src/db/model/document.rs
+++ crates/content-base/src/db/model/document.rs
@@ -3,3 +3,10 @@ use crate::db::model::PageModel;
 pub struct DocumentModel {
     pub data: Vec<PageModel>,
 }
+
+impl DocumentModel {
+    pub fn new(data: Vec<PageModel>) -> Self {
+        Self { data }
+    }
+    
+}
