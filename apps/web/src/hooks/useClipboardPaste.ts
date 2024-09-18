3c7971a006de55622a1b204905c79bd1c9a67561
web3nomad.eth <me@web3nomad.eth>
2024-09-18 14:53:10 +0800

    remove tauri-plugin-clipboard

--- apps/web/src/hooks/useClipboardPaste.ts
+++ apps/web/src/hooks/useClipboardPaste.ts
@@ -1,17 +1,16 @@
 import { useCallback, useEffect, useState } from 'react'
-import clipboard from 'tauri-plugin-clipboard-api'
 
 export const useClipboardPaste = () => {
   const [filesPasted, setFilesPasted] = useState<string[]>([])
   const handlePaste = useCallback(async (e: ClipboardEvent) => {
     const files: string[] = []
-    try {
-      const res: string[] = await clipboard.readFiles()
-      res.forEach((item) => {
-        // clipboard 里面的文件名会被 encode，需要解码一下
-        files.push(window.decodeURIComponent(item))
-      })
-    } catch (e) {}
+    // try {
+    //   const res: string[] = await clipboard.readFiles()
+    //   res.forEach((item) => {
+    //     // clipboard 里面的文件名会被 encode，需要解码一下
+    //     files.push(window.decodeURIComponent(item))
+    //   })
+    // } catch (e) {}
     // 只阻拦文件
     if (files.length > 0) {
       e.preventDefault()
