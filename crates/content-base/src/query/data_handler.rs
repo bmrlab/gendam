2abccf4a9e7a0716031512d66d0543f7375b7b37
ZingerLittleBee <6970999@gmail.com>
2024-09-23 16:12:20 +0800

    expand select result

--- crates/content-base/src/query/data_handler.rs
+++ crates/content-base/src/query/data_handler.rs
@@ -1,14 +1,20 @@
 use ai::TextEmbeddingModel;
 use regex::Regex;
 
+use super::{model::SearchModel, QueryPayload};
+use crate::db::model::{PayloadModel, SelectResultModel};
+use crate::query::payload::audio::AudioSearchMetadata;
+use crate::query::payload::image::ImageSearchMetadata;
+use crate::query::payload::raw_text::RawTextSearchMetadata;
+use crate::query::payload::web_page::WebPageSearchMetadata;
+use crate::query::payload::{SearchMetadata, SearchResultData};
 use crate::{
+    concat_arrays,
     constant::STOP_WORDS,
     query::model::{TextSearchModel, TextToken},
     utils::deduplicate,
     ContentBase,
 };
-
-use super::{model::SearchModel, QueryPayload};
 
 impl ContentBase {
     /// 构造内部查询模型 SearchModel
@@ -57,5 +63,77 @@ impl ContentBase {
             .collect();
 
         Ok(deduplicate(tokens))
+    }
+
+    pub async fn expand_select_result(
+        &self,
+        select_result: &SelectResultModel,
+        score: f32,
+        payload: &PayloadModel,
+    ) -> anyhow::Result<Vec<SearchResultData>> {
+        let file_identifier = payload.file_identifier();
+        let metadata = match select_result {
+            SelectResultModel::Image(_) => vec![SearchMetadata::Image(ImageSearchMetadata {})],
+            SelectResultModel::Audio(audio) => audio
+                .audio_frame
+                .iter()
+                .map(|frame| {
+                    SearchMetadata::Audio(AudioSearchMetadata {
+                        start_timestamp: frame.start_timestamp as i64,
+                        end_timestamp: frame.end_timestamp as i64,
+                    })
+                })
+                .collect(),
+            SelectResultModel::Video(video) => {
+                let audio_metadata = video
+                    .audio_frame
+                    .iter()
+                    .map(|frame| {
+                        SearchMetadata::Audio(AudioSearchMetadata {
+                            start_timestamp: frame.start_timestamp as i64,
+                            end_timestamp: frame.end_timestamp as i64,
+                        })
+                    })
+                    .collect::<Vec<SearchMetadata>>();
+                let image_metadata = video
+                    .image_frame
+                    .iter()
+                    .map(|_| SearchMetadata::Image(ImageSearchMetadata {}))
+                    .collect::<Vec<SearchMetadata>>();
+
+                concat_arrays!(audio_metadata, image_metadata).into_vec()
+            }
+            SelectResultModel::WebPage(web_page) => web_page
+                .page
+                .iter()
+                .map(|page| {
+                    SearchMetadata::WebPage(WebPageSearchMetadata {
+                        start_index: page.start_index as usize,
+                        end_index: page.end_index as usize,
+                    })
+                })
+                .collect(),
+            SelectResultModel::Document(document) => document
+                .page
+                .iter()
+                .map(|page| {
+                    SearchMetadata::RawText(RawTextSearchMetadata {
+                        start_index: page.start_index as usize,
+                        end_index: page.end_index as usize,
+                    })
+                })
+                .collect(),
+            _ => {
+                vec![]
+            }
+        };
+        Ok(metadata
+            .into_iter()
+            .map(|metadata| SearchResultData {
+                file_identifier: file_identifier.clone(),
+                score,
+                metadata,
+            })
+            .collect())
     }
 }
