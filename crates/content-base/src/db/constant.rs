70747f565c507009f868c6c2021ef86d2b4dab6e
ZingerLittleBee <6970999@gmail.com>
2024-09-30 10:36:07 +0800

    init db use surrealdb

---
+++ crates/content-base/src/db/constant.rs
@@ -0,0 +1,7 @@
+// env
+pub const DATABASE_HOST: &str = "DATABASE_HOST";
+pub const DATABASE_PORT: &str = "DATABASE_PORT";
+pub const DATABASE_USER: &str = "DATABASE_USER";
+pub const DATABASE_PASSWORD: &str = "DATABASE_PASSWORD";
+pub const DATABASE_NS: &str = "DATABASE_NS";
+pub const DATABASE_NAME: &str = "DATABASE_NAME";
