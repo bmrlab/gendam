52ce80d03988a36d3c8705efa1c3c412270952e3
web3nomad.eth <me@web3nomad.eth>
2024-10-21 22:57:17 +0800

    fork candle_nn::Sequential to make it thread safe, use enum to replace trait object

--- crates/ai/src/llava_phi3_mini/mod.rs
+++ crates/ai/src/llava_phi3_mini/mod.rs
@@ -1,4 +1,9 @@
+mod quantized_llava_phi3;
+use quantized_llava_phi3::{format_prompt, load_image, QLLaVAPhi3};
+
 use crate::traits::{ImageCaptionInput, ImageCaptionOutput, Model};
+
+use candle_core::Device;
 use std::path::Path;
 
 impl Model for LLaVAPhi3Mini {
@@ -18,15 +23,30 @@ impl Model for LLaVAPhi3Mini {
     }
 }
 
-pub struct LLaVAPhi3Mini {}
+pub struct LLaVAPhi3Mini {
+    qllavaphi3: QLLaVAPhi3,
+}
 
 impl LLaVAPhi3Mini {
     pub async fn new(
-        _device: &str,
-        _gguf_model_path: impl AsRef<Path>,
-        _mmproj_gguf_model_path: impl AsRef<Path>,
-        _tokenizer_path: impl AsRef<Path>,
+        device: &str,
+        gguf_model_path: impl AsRef<Path>,
+        mmproj_gguf_model_path: impl AsRef<Path>,
+        tokenizer_path: impl AsRef<Path>,
     ) -> anyhow::Result<Self> {
-        Ok(Self {})
+        let device = match device {
+            "metal" => Device::new_metal(0)?,
+            _ => anyhow::bail!("Unsupported device: {}", device),
+        };
+        let qllavaphi3 = QLLaVAPhi3::load(
+            &device,
+            gguf_model_path,
+            mmproj_gguf_model_path,
+            tokenizer_path,
+        )?;
+        Ok(Self {
+            //
+            qllavaphi3,
+        })
     }
 }
