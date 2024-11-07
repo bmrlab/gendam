dc4a2f6ba265d5c8f185cba7ed698cba28762848
web3nomad.eth <me@web3nomad.eth>
2024-11-07 09:35:20 +0800

    organize code in standalone.rs

--- apps/api-server/src/standalone.rs
+++ apps/api-server/src/standalone.rs
@@ -31,57 +31,54 @@ pub async fn start_server() {
         // tracing::error!("This event will be logged in the root span.");
     }
 
-    let local_data_root = match env::var("LOCAL_DATA_DIR") {
-        Ok(path) => Path::new(&path).to_path_buf(),
-        Err(_e) => {
-            // #[cfg(not(debug_assertions))]
-            // {}
-            panic!("'$LOCAL_DATA_DIR' is not set ({})", _e)
-        }
+    let ctx = {
+        let local_data_root = match env::var("LOCAL_DATA_DIR") {
+            Ok(path) => Path::new(&path).to_path_buf(),
+            Err(_e) => {
+                // #[cfg(not(debug_assertions))]
+                // {}
+                panic!("'$LOCAL_DATA_DIR' is not set ({})", _e)
+            }
+        };
+        std::fs::create_dir_all(&local_data_root).unwrap();
+
+        let resources_dir = match env::var("LOCAL_RESOURCES_DIR") {
+            Ok(path) => Path::new(&path).to_path_buf(),
+            Err(_e) => {
+                panic!("'$LOCAL_RESOURCES_DIR' is not set ({})", _e)
+            }
+        };
+
+        let temp_dir = std::env::temp_dir();
+        let cache_dir = temp_dir.clone();
+        // 本地开发环境, cache_dir 直接和 temp_dir 共享一个目录, 暂时不会有问题
+
+        let mut default_store = Store::new(local_data_root.join("settings.json"));
+        default_store.load().unwrap_or_else(|e| {
+            tracing::warn!("Failed to load store: {:?}", e);
+        });
+
+        let store = Arc::new(Mutex::new(default_store));
+
+        let node = Arc::new(Mutex::<p2p::Node<p2p_info::ShareInfo>>::new(
+            p2p::Node::new().expect("create node error"),
+        ));
+
+        Ctx::<Store>::new(
+            local_data_root,
+            resources_dir,
+            temp_dir,
+            cache_dir,
+            store,
+            node,
+        )
     };
-    std::fs::create_dir_all(&local_data_root).unwrap();
-
-    let resources_dir = match env::var("LOCAL_RESOURCES_DIR") {
-        Ok(path) => Path::new(&path).to_path_buf(),
-        Err(_e) => {
-            panic!("'$LOCAL_RESOURCES_DIR' is not set ({})", _e)
-        }
-    };
-
-    let temp_dir = std::env::temp_dir();
-    let cache_dir = temp_dir.clone();
-    // 本地开发环境, cache_dir 直接和 temp_dir 共享一个目录, 暂时不会有问题
-
-    let mut default_store = Store::new(local_data_root.join("settings.json"));
-    default_store.load().unwrap_or_else(|e| {
-        tracing::warn!("Failed to load store: {:?}", e);
-    });
-
-    let cors = CorsLayer::new()
-        .allow_methods(Any)
-        .allow_headers(Any)
-        .allow_origin(Any);
-
-    let store = Arc::new(Mutex::new(default_store));
-    let router = get_rspc_routes::<Ctx<Store>>().arced();
-
-    let node = Arc::new(Mutex::<p2p::Node<p2p_info::ShareInfo>>::new(
-        p2p::Node::new().expect("create node error"),
-    ));
-
-    let ctx = Ctx::<Store>::new(
-        local_data_root,
-        resources_dir,
-        temp_dir,
-        cache_dir,
-        store,
-        node,
-    );
 
     let app: axum::Router = axum::Router::new()
         .route("/", get(|| async { "Hello 'rspc'!" }))
         .nest("/rspc", {
-            rspc_axum::endpoint(router.clone(), {
+            let router = get_rspc_routes::<Ctx<Store>>().arced();
+            rspc_axum::endpoint(router, {
                 let ctx = ctx.clone();
                 move |_parts: Parts| {
                     // tracing::info!("Client requested operation '{}'", parts.uri.path());
@@ -91,15 +88,26 @@ pub async fn start_server() {
             })
         })
         .merge(localhost::get_routes(ctx.clone()))
-        .layer(cors);
+        .layer({
+            let cors = CorsLayer::new()
+                .allow_methods(Any)
+                .allow_headers(Any)
+                .allow_origin(Any);
+            cors
+        });
 
-    let addr = "[::]:3001".parse::<std::net::SocketAddr>().unwrap(); // This listens on IPv6 and IPv4
-    tracing::debug!("Listening on http://{}/rspc/version", addr);
-    let listener = tokio::net::TcpListener::bind(addr).await.unwrap();
-    axum::serve(listener, app)
-        .with_graceful_shutdown(shutdown_signal(ctx.clone()))
-        .await
-        .unwrap();
+    axum::serve(
+        {
+            let addr = "[::]:3001".parse::<std::net::SocketAddr>().unwrap(); // This listens on IPv6 and IPv4
+            tracing::debug!("Listening on http://{}/rspc/version", addr);
+            let listener = tokio::net::TcpListener::bind(addr).await.unwrap();
+            listener
+        },
+        app,
+    )
+    .with_graceful_shutdown(shutdown_signal(ctx.clone()))
+    .await
+    .unwrap();
 }
 
 async fn shutdown_signal(ctx: impl CtxWithLibrary) {
